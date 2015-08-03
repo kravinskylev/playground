07cbb77908409ae218158db52f8806815de7ec13
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-03 16:03:02 -0600

    working proof of concept

--- method_storage.rb
+++ method_storage.rb
@@ -1,20 +1,18 @@
 class Foo
   def bar(arg, &block)
-    block.call arg
+    block.call(arg)     # => 15, 15
   end
 end
 
-foo = Foo.new
-foo.bar(10) { |e| e + 5 } #=> 15
+foo = Foo.new              # => #<Foo:0x007fc9038361b0>
+foo.bar(10) { |e| e + 5 }  # => 15
 
-FOO = foo.method(:bar).to_proc.call
+FOO = foo.method(:bar).to_proc  # => #<Proc:0x007fc903835cb0 (lambda)>
 
-foo.class.send(:define_method, :bar) { |arg| "this is a stubbed method" }
+foo.class.send(:define_method, :bar) { |arg| "this is a stubbed method" }  # => :bar
 
-foo.bar(10) { |e| e + 5 } #=> "this is a stubbed method"
+foo.bar(10) { |e| e + 5 }  # => "this is a stubbed method"
 
-foo.class.send(:define_method, :bar) { |arg| FOO }
+foo.class.send(:define_method, :bar) { |arg, &block| FOO.call(arg, &block) }  # => :bar
 
-foo.bar(10) { |e| e + 5 } #=> 15
-
-
+foo.bar(10) { |e| e + 5 }  # => 15

9162e343da592a87944d80ad901351fb00bdec0c
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-03 16:59:46 -0600

    reset works for everything but previously undefined singleton methods

--- method_storage.rb
+++ method_storage.rb
@@ -1,18 +1,51 @@
 class Foo
   def bar(arg, &block)
-    block.call(arg)     # => 15, 15
+    block.call(arg)
+  end
+
+  def foobar
+    "foobar"
   end
 end
 
-foo = Foo.new              # => #<Foo:0x007fc9038361b0>
-foo.bar(10) { |e| e + 5 }  # => 15
+class Object
+  def stubber(hash)
+    hash.each do |name, value|
+      if methods.include?(name)
+        instance_variable_set("@lobster_#{name}", method(name).to_proc)
+      end
+      self.send(:define_singleton_method, name) { value }
+    end
+  end
 
-FOO = foo.method(:bar).to_proc  # => #<Proc:0x007fc903835cb0 (lambda)>
+  def reset
+    singleton_methods.each do |method|
+      unless instance_variable_get("@lobster_#{method}") == nil
+        instance_method = instance_variable_get("@lobster_#{method}")
+        send(:define_singleton_method, method) { |*arg, &block| instance_method.call(*arg, &block) }
+      else
+        self
+        method
+        self.instance_eval { undef method }
+        p "im being evalled"
+      end
+    end
+  end
+end
 
-foo.class.send(:define_method, :bar) { |arg| "this is a stubbed method" }  # => :bar
+foo = Foo.new
+foo.bar(10) { |e| e + 5 }
+foo.foobar
+foo.stubber(:bar => "new method",
+            :foobar => "barfoo",
+            :new_method => "new value")
+foo.bar
+foo.foobar
+foo.new_method
+foo.reset
+foo.bar(10) {|e| e + 5}
+foo.foobar
+foo.new_method
 
-foo.bar(10) { |e| e + 5 }  # => "this is a stubbed method"
-
-foo.class.send(:define_method, :bar) { |arg, &block| FOO.call(arg, &block) }  # => :bar
-
-foo.bar(10) { |e| e + 5 }  # => 15
+foo.instance_eval { undef :new_method }
+foo.new_method
